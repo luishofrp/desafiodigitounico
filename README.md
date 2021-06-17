fc9597a9ad09e7aaa556d1bd47f1b45326e46d88
luishofrp <lusihof@gmail.com>
2021-06-17 19:14:44 -0300

    Criação Desafio Digito Unico

---
+++ README.md
@@ -0,0 +1,13 @@
+# Desafio Digito Unico
+
+Este repositório é para resolver o seguinte desafio:
+
+
+
+![desafio.png](desafio.png)
+
+
+
+Aplicação Executando:
+
+![image-20210617191058417](print_aplicacao.png)
---
+++ main/digitoUnicoT.java
@@ -0,0 +1,44 @@
+package main;
+
+import java.util.Scanner;
+
+public class digitoUnicoT {
+    public static int somaNumero(int n, int somaN) {
+        if (n < 1)
+            return somaN;
+        somaN += (n % 10);
+        return somaNumero(n / 10, somaN);
+
+
+    }
+        public static int somaTotal(int multnk, int somaT) {
+            if (multnk < 1)
+                return somaT;
+            somaT += (multnk % 10);
+            return somaTotal(multnk / 10, somaT);
+
+
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Bem Vindo a Calculadora de Digito Unico");
+            System.out.println("Digite um numero (n): ");
+            int n = new Scanner(System.in).nextInt();
+            System.out.println("Digite um numero (K): ");
+            int k = new Scanner(System.in).nextInt();
+            if (n > 9) {
+                int soma = somaNumero(n, 0);
+                int multnk = soma * k;
+                int somatotal = somaTotal(multnk, 0);
+                System.out.printf("========================================\n");
+                System.out.println("digitoUnico("+multnk+") = " + somatotal);
+                System.out.printf("========================================\n");
+            } else {
+            }
+        }
+    }
+
+
+
+
+
